538125934b98f2f036523486dea9f461818a50ac
rana-abhi777 <abhisekrana777@gmail.com>
2020-08-11 18:27:39 +0530

    learned arrays

---
+++ src/basics_of_java/data_types/Array_types.java
@@ -0,0 +1,19 @@
+package basics_of_java.data_types;
+
+public class Array_types {
+
+	public static void main(String[] args) {
+		// declaration of an array
+		int[] arr1;
+		
+		// initialization of an array
+		arr1 = new int[5];
+		for (var i: arr1) System.out.println(i);
+
+		// another direct approach for above 2 steps
+		int arr2[] = new int[5];
+		for (var i: arr2) System.out.print(i + " : ");
+		
+	}
+
+}
