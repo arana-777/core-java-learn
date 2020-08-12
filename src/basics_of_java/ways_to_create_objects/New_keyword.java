b1a1d239b7c784d7dc69d12a2a37142109ea07cb
rana-abhi777 <abhisekrana777@gmail.com>
2020-08-12 09:26:30 +0530

    learned new keyword to creat objects

---
+++ src/basics_of_java/ways_to_create_objects/New_keyword.java
@@ -0,0 +1,12 @@
+package basics_of_java.ways_to_create_objects;
+
+public class New_keyword {
+	String s = "New keyword example";
+
+	public static void main(String[] args) {
+		New_keyword obj = new New_keyword();
+		System.out.println(obj.s);
+
+	}
+
+}
