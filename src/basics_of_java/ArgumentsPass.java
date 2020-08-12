b34e26f5c93e175516f93bfc47954b67c55c2d01
rana-abhi777 <abhisekrana777@gmail.com>
2020-08-12 09:25:35 +0530

    reading arguments in java

---
+++ src/basics_of_java/ArgumentsPass.java
@@ -0,0 +1,12 @@
+package basics_of_java;
+
+public class ArgumentsPass {
+
+	public static void main(String[] args) {
+		for (var i=0; i<args.length; i++) {
+			System.out.print(args[i] + " ");
+		}
+
+	}
+
+}
